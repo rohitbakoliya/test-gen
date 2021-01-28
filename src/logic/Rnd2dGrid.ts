d41fef205d962c62170792b11c7e997953314680
rohitbakoliya <bakoliyarohit00@gmail.com>
2021-01-28 16:03:58 +0530

    2d grid gen added with tests

--- src/logic/Rnd2dGrid.ts
+++ src/logic/Rnd2dGrid.ts
@@ -1,25 +1,40 @@
+import Random from '../helper/Random';
 import RndNumber from './RndNumber';
 import RndString from './RndString';
 
-interface RndArrayParams {
-  dim: [number, number];
+export interface Rnd2dGridParams {
+  minDim: [number, number];
+  maxDim: [number, number];
   // for array of strings or patterns
   pattern?: RegExp | string;
   // for array of numbers
   range?: [number, number];
 }
+export type Grid = (rnd2dGridParams: Rnd2dGridParams) => Array<Array<number | string>>;
 
-const Rnd2dGrid = (rndArrayParams: RndArrayParams): Array<Array<number | string>> => {
-  const { dim, pattern, range } = rndArrayParams;
-  const [row, col] = dim;
+/**
+ * Random 2D grid generator either with regex or numeric range
+ * @returns 2D grid within provided dimention range
+ */
+
+const Rnd2dGrid: Grid = ({ minDim, maxDim, pattern, range }) => {
+  const [minRow, minCol] = minDim;
+  const [maxRow, maxCol] = maxDim;
+  const row = Random({ min: minRow, max: maxRow });
+  const col = Random({ max: maxCol, min: minCol });
+
   const arr: Array<Array<number | string>> = Array.from(Array(row), () => new Array(col));
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
       if (pattern !== undefined) {
         arr[i][j] = RndString(pattern);
       } else {
-        const [min, max] = range!;
-        arr[i][j] = RndNumber({ min, max });
+        if (range === undefined) {
+          throw new Error('Either pattern or range must be provided');
+        } else {
+          const [min, max] = range;
+          arr[i][j] = RndNumber({ min, max });
+        }
       }
     }
   }
---
+++ src/tests/Rnd2dGrid.test.ts
@@ -0,0 +1,39 @@
+import Rnd2dGrid from '../logic/Rnd2dGrid';
+
+describe('Random array tests', () => {
+  test('should return empty array', () => {
+    const result = Rnd2dGrid({ minDim: [0, 0], maxDim: [0, 0] });
+    expect(result).toBeArray();
+    expect(result).toBeArrayOfSize(0);
+  });
+  const minDim: [number, number] = [2, 10];
+  const maxDim: [number, number] = [100, 400];
+  const result = Rnd2dGrid({ minDim, maxDim, pattern: /Alice|Bob/ });
+  const size = result.length;
+  test('Pattern => should return array of correct size', () => {
+    expect(result).toBeArray();
+    expect(size).toBeWithin(minDim[0], maxDim[0] + 1);
+  });
+  test('Pattern => should return correct array elements with correct size', () => {
+    result.forEach(arr => {
+      expect(arr).toBeArray();
+      expect(arr.length).toBeWithin(minDim[1], maxDim[1] + 1);
+      expect(arr.every(value => value === 'Bob' || value === 'Alice')).toBeTrue();
+    });
+  });
+
+  const range: [number, number] = [-10, 50];
+  const result1 = Rnd2dGrid({ minDim, maxDim, range });
+  const size1 = result1.length;
+  test('Range => should return array of correct size', () => {
+    expect(result1).toBeArray();
+    expect(size1).toBeWithin(minDim[0], maxDim[0] + 1);
+  });
+  test('Range => should return correct array elements with correct size', () => {
+    result1.forEach(arr => {
+      expect(arr).toBeArray();
+      expect(arr.length).toBeWithin(minDim[1], maxDim[1] + 1);
+      expect(arr.every(ele => ele >= range[0] && ele <= range[1])).toBeTrue();
+    });
+  });
+});
